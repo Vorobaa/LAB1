3c5bdc65886e436072172ef3016d3d41a8d2e06a
Vorobaa <vorobaaa72@gmail.com>
2026-05-29 21:43:12 +0300

    js add

--- form.js
+++ form.js
@@ -1,6 +1,32 @@
 const form = document.getElementById("contact-form");
+const messageField = document.getElementById("message");
 
-form.addEventListener("submit", function (event){
+messageField.addEventListener("input", function () {
+    if (this.value.toLowerCase().includes('спам'))
+    {
+        this.setCustomValidity('Будь ласка, уникайте спаму у повідомленні');
+    }
+    else
+    {
+        this.setCustomValidity('');
+    }
+});
+
+form.addEventListener("submit", function (event) {
+    if (!form.checkValidity())
+    {
+        event.preventDefault();
+        form.reportValidity();
+        return;
+    }
     event.preventDefault();
+
+    const data = new FormData(form);
+    const formObject = Object.fromEntries(data.entries());
+
+    console.log('Дані готові до відправки на сервер:', formObject);
+    alert('Повідомлення успішно відправлено!');
+
+    form.reset();
 });
 
